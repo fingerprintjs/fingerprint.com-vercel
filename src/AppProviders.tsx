b0911a4d944130ee5bf6356ce143b9870b55a08a
natapg <natanael.galfrascoli@fingerprintjs.com>
2022-02-22 20:01:27 -0300

    migrated to react wrapper

--- src/AppProviders.tsx
+++ src/AppProviders.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import { FormProvider } from './hooks/useForm'
-import { FpjsProvider } from './context/FpjsContext'
+import { FpjsProvider } from '@fingerprintjs/fingerprintjs-pro-react'
+import * as FingerprintJS from '@fingerprintjs/fingerprintjs-pro'
 import { GithubProvider } from './context/GithubContext'
 import { HistoryListener } from './context/HistoryListener'
+import { FPJS_PUBLIC_TOKEN, FPJS_REGION, FPJS_ENDPOINT } from './constants/env'
 
 export type Props = {
   children: React.ReactNode
 }
 
 export default function AppProviders({ children }: Props) {
+  const publicToken = FPJS_PUBLIC_TOKEN
+  const region = FPJS_REGION as FingerprintJS.Region
+  const endpoint = FPJS_ENDPOINT
   return (
-    <FpjsProvider>
+    <FpjsProvider
+      loadOptions={{
+        token: publicToken,
+        region,
+        endpoint,
+      }}
+    >
       <FormProvider>
         <GithubProvider>
           <HistoryListener>
