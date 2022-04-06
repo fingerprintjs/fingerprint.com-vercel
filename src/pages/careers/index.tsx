4819ed363969c1c2937a2766d3b29e2289ed9e86
Natanael Pablo Galfrascoli <47128448+natapg@users.noreply.github.com>
2022-04-06 14:42:07 -0300

    Careers Page: First two sections (#573)

--- src/pages/careers/index.tsx
+++ src/pages/careers/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { LayoutTemplate } from '../../components/Layout'
 import BreadcrumbsSEO from '../../components/Breadcrumbs/BreadcrumbsSEO'
+import HeroSection from '../../components/HeroWithCTA/HeroWithCTA'
+import MissionAndVisionSection from '../../components/careers/MissionAndVisionSection/MissionAndVisionSection'
+import { URL } from '../../constants/content'
 
 import { GeneratedPageContext } from '../../helpers/types'
 
 import useSiteMetadata from '../../hooks/useSiteMetadata'
 import { useLocation } from '@reach/router'
+
+import styles from './Careers.module.scss'
 
 interface GitHubProps {
   pageContext: GeneratedPageContext
@@ -23,6 +28,17 @@ export default function GitHubPage({ pageContext }: GitHubProps) {
   return (
     <LayoutTemplate siteMetadata={siteMetadata}>
       {breadcrumbs && <BreadcrumbsSEO breadcrumbs={breadcrumbs} />}
+      <HeroSection
+        className={styles.heroSection}
+        title='Join Our Team'
+        ctaText='Explore our jobs'
+        ctaHref={URL.careersUrl}
+        openNewTab
+        variant='secondary'
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </HeroSection>
+      <MissionAndVisionSection />
     </LayoutTemplate>
   )
 }
