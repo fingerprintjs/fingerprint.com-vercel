cf6ccbd27f927c576a8273eff747a36e65e420b8
Natanael Pablo Galfrascoli <47128448+natapg@users.noreply.github.com>
2022-11-28 11:23:03 -0300

    homepage: platform section (#827)
    Closes https://github.com/fingerprintjs/fingerprint.com/issues/795

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Layout } from '../components/Layout'
 import HeroSection from '../components/homepage/HeroSection/HeroSection'
 import ClientsSection from '../components/homepage/ClientsSection/ClientsSection'
 import DemoSection from '../components/homepage/DemoSection/DemoSection'
+import PlatformSection from '../components/homepage/PlatformSection/PlatformSection'
 import { SEO } from '../components/SEO/SEO'
 
 export default function IndexPage() {
@@ -12,6 +13,7 @@ export default function IndexPage() {
       <HeroSection />
       <ClientsSection />
       <DemoSection />
+      <PlatformSection />
     </Layout>
   )
 }
